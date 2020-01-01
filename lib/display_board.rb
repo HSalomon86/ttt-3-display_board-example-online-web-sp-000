c5c9f23c32f606964967b51d63ca34528071bcdd
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-01 22:56:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   cell = "   "
-  row = "#{cell}#{separator}#{cell}#{separator}#{cell}"
   lines = "-----------"
   return puts row puts lines puts row puts lines puts row
 
